25b8fc58d87b55c7e2e7250a0b8c7255031cb4cb
Michael Wuori <michael.wuori@gmail.com>
2022-02-02 00:53:31 -0500

    Adds character animation on completion.

--- src/components/grid/CompletedRow.tsx
+++ src/components/grid/CompletedRow.tsx
@@ -1,5 +1,6 @@
 import { getGuessStatuses } from '../../lib/statuses'
 import { Cell } from './Cell'
+import { useEffect } from 'react'
 
 type Props = {
   guess: string
@@ -8,6 +9,20 @@ type Props = {
 export const CompletedRow = ({ guess }: Props) => {
   const statuses = getGuessStatuses(guess)
 
+  useEffect(() => {
+    // Show completed row cell-by-cell
+    const cellShowDelay = 400;
+    const cellsContainer = document.querySelectorAll('.completed:not(.displayed)') ?? [];
+    if(cellsContainer[0]){
+      Array.from(cellsContainer[0].children).forEach((cell, i) => {
+        setTimeout(() => {
+            cell.classList.add('shown');
+        }, cellShowDelay*i);
+      });
+      cellsContainer[0].classList.add('displayed');
+    }
+  });
+  
   return (
     <div className="flex justify-center mb-1">
       {guess.split('').map((letter, i) => (
