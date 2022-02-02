1cb16a0194aa5e558262a80f8680d371350c950f
Michael Wuori <michael.wuori@gmail.com>
2022-02-02 01:16:30 -0500

    Updates alert styling.

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const Alert = ({ isOpen, message, variant = 'warning' }: Props) => {
   const classes = classNames(
-    'fixed top-5 left-1/2 transform -translate-x-1/2 max-w-sm w-full shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden',
+    'shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden',
     {
       'bg-rose-200': variant === 'warning',
       'bg-blue-200 z-20': variant === 'success',
@@ -28,11 +28,13 @@ export const Alert = ({ isOpen, message, variant = 'warning' }: Props) => {
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
     >
-      <div className={classes}>
-        <div className="p-4">
-          <p className="text-sm text-center font-medium text-gray-900">
-            {message}
-          </p>
+      <div className="alert">
+        <div className={classes}>
+          <div className="p-4">
+            <p className="text-lg text-center font-medium text-gray-900">
+              {message}
+            </p>
+          </div>
         </div>
       </div>
     </Transition>
